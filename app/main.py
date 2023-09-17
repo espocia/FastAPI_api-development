0eb326c9fbfd252590aaa59dc7335bf7344aeda9
espocia <joshuajonellegarbin.espocia@gmail.com>
2023-09-17 12:27:31 +0800

    cors origin -> *

--- app/main.py
+++ app/main.py
@@ -17,8 +17,7 @@ post_manager = PostManager()
 app.add_middleware(
     CORSMiddleware,
     # Specify the origin(s) you want to allow
-    allow_origins=["https://dulcet-melba-efe48a.netlify.app",
-                   "http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  # You can specify the allowed HTTP methods
     allow_headers=["*"],  # You can specify the allowed headers
