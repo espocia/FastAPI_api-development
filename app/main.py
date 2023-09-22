98886d8e48e7c3612d2356e995472c43610e34ab
JJ <joshuajonellegarbin.espocia@gmail.com>
2023-09-22 17:37:59 +0800

    s3 bucket file upload

--- app/main.py
+++ app/main.py
@@ -3,11 +3,15 @@ from app.database import PostManager
 from app.basemodel import AddressDegree, PersonalInfo,  Status, File
 
 # FastAPI import -------------------------
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse, FileResponse
 
 # feature --------------------------------
 from app.mailer import confim_application
+from botocore.exceptions import NoCredentialsError
+import boto3
+import os
 
 app = FastAPI(title="GUTZ online application services  API")
 
@@ -25,6 +29,18 @@ app.add_middleware(
 
 # Add a middleware to handle mixed content
 
+AWS_ACCESS_KEY_ID = os.environ.get("ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("SECRET_ACCESS_KEY")
+AWS_BUCKET_NAME = os.environ.get("BUCKET_NAME")
+AWS_DIR = "resume/"
+
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+)
+
 
 @app.get("/")
 async def root():
@@ -32,6 +48,44 @@ async def root():
     Welcome endpoint
     """
     return {"message": "Welcome to my API"}
+
+
+@app.post("/uploadfile/")
+async def upload_file(file: UploadFile):
+    try:
+        # Upload file to S3 bucket
+        object_key = AWS_DIR + file.filename
+        s3.upload_fileobj(
+            file.file,
+            AWS_BUCKET_NAME,
+            object_key
+        )
+        return JSONResponse(content={"message": "File uploaded successfully"}, status_code=200)
+    except NoCredentialsError:
+        return JSONResponse(content={"error": "AWS credentials not found"}, status_code=500)
+
+
+@app.get("/downloadfile/{file_name}")
+async def download_file(file_name: str):
+    try:
+        # Download file from S3 bucket to the server's local filesystem
+        # Provide the local path where you want to save the file
+        local_file_path = f"/tmp/{file_name}"  # Use a temporary directory
+        s3.download_file(
+            AWS_BUCKET_NAME,
+            file_name,
+            local_file_path
+        )
+
+        # Serve the downloaded file as a response for download
+        return FileResponse(
+            local_file_path,
+            headers={
+                "Content-Disposition": f"attachment; filename={file_name}"
+            }
+        )
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
 
 
 @app.get("/posts")
